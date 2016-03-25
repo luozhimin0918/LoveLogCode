9c0926a561f0743eb3dda76fc7e4041c85937a00
luozhimin0918 <838111691@qq.com>
2016-03-25 15:04:43 +0800

    3月25 13.59

--- LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
+++ LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
@@ -3,25 +3,19 @@ package com.smarter.LoveLog.activity;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
-import android.view.ContextThemeWrapper;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 import com.alibaba.fastjson.JSON;
 import com.android.volley.Request;
@@ -32,25 +26,22 @@ import com.smarter.LoveLog.R;
 import com.smarter.LoveLog.db.AppContextApplication;
 import com.smarter.LoveLog.db.SharedPreferences;
 import com.smarter.LoveLog.http.FastJsonRequest;
-import com.smarter.LoveLog.model.category.InvitationDataDeatil;
 import com.smarter.LoveLog.model.home.DataStatus;
 import com.smarter.LoveLog.model.loginData.LoginDataActi;
 import com.smarter.LoveLog.model.loginData.LoginDataInfo;
-import com.smarter.LoveLog.ui.popwindow.BabyPopWindow;
-import com.smarter.LoveLog.ui.productViewPager.AutoLoopViewPager;
-import com.smarter.LoveLog.ui.productViewPager.CirclePageIndicator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2015/11/30.
  */
-public class LoginActivity extends BaseFragmentActivity implements View.OnClickListener{
-    String Tag= "LoginActivity";
+public class LoginActivity extends BaseFragmentActivity implements View.OnClickListener {
+    String Tag = "LoginActivity";
     @Bind(R.id.logon)
     LinearLayout logon;
     @Bind(R.id.linearSeePassword)
@@ -69,13 +60,15 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
     LinearLayout forgetPass;
 
 
+    Context mContext;
 
-
-
-   Context mContext;
-
-   RequestQueue mQueue;
-
+    RequestQueue mQueue;
+    @Bind(R.id.weixinLogin)
+    ImageView weixinLogin;
+    @Bind(R.id.qqLogin)
+    ImageView qqLogin;
+    @Bind(R.id.xinlanLogin)
+    ImageView xinlanLogin;
 
 
     @Override
@@ -86,8 +79,8 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
         setContentView(R.layout.activity_login_view);
         ButterKnife.bind(this);
         getDataIntent();
-        mContext=this;
-        mQueue =  AppContextApplication.getInstance().getmRequestQueue();
+        mContext = this;
+        mQueue = AppContextApplication.getInstance().getmRequestQueue();
         intData();
         setListen();
 
@@ -109,68 +102,32 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
 
     private void getDataIntent() {
         Intent intent = getIntent();
-        if(intent!=null){
-            String  str = intent.getStringExtra("ObjectData");
-           // Toast.makeText(this,str+"",Toast.LENGTH_SHORT).show();
+        if (intent != null) {
+            String str = intent.getStringExtra("ObjectData");
+            // Toast.makeText(this,str+"",Toast.LENGTH_SHORT).show();
         }
 
 
     }
 
-   Boolean  isShowPassword=false;
-    @Override
-    public void onClick(View v) {
-         switch (v.getId()){
-             case R.id.linearSeePassword:
-                    if(!isShowPassword){
+    Boolean isShowPassword = false;
 
-                        //如果选中，显示密码
-                        password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                        isShowPassword=true;
-                    }else{
-                        password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                        isShowPassword=false;
-                    }
-                 break;
-             case  R.id.logon:
-                 LoginBut(phone.getText().toString(),password.getText().toString(),"","");
-                 break;
-             case R.id.backBut:
-                 finish();
-                 break;
-             case  R.id.regitstBut:
-                 //跳转到注册界面
-                 Intent intent = new Intent(mContext, QuaiRegitedActivity.class);
-              /*  Bundle bundle = new Bundle();
-                bundle.putSerializable("PromotePostsData", (Serializable) pp);
-                intent.putExtras(bundle);*/
-                 mContext.startActivity(intent);
-                 break;
-             case  R.id.forgetPass:
-                 //跳转到注册界面
-                 Intent intent2 = new Intent(mContext, FindPasswordActivity.class);
-              /*  Bundle bundle = new Bundle();
-                bundle.putSerializable("PromotePostsData", (Serializable) pp);
-                intent.putExtras(bundle);*/
-                 mContext.startActivity(intent2);
-                 break;
 
-         }
-    }
 
-    public LoginDataActi  loginDataActi;
-    private void LoginBut( final String user, final String pass,String uid,String sid) {
+    public LoginDataActi loginDataActi;
+
+    private void LoginBut(final String user, final String pass, String uid, String sid) {
         String url = "http://mapp.aiderizhi.com/?url=/user/signin";//
-        Map<String, String>   mapTou = new HashMap<String, String>();
-        String  sessinStr ="{\"session\":{\"uid\":\""+uid+"\",\"sid\":\""+sid+"\"}}";
+        Map<String, String> mapTou = new HashMap<String, String>();
+        String sessinStr = "{\"session\":{\"uid\":\"" + uid + "\",\"sid\":\"" + sid + "\"}}";
         mapTou.put("json", sessinStr);
 
 
         Map<String, String> map = new HashMap<String, String>();
-        String  value="{\"account\":\""+user+"\",\"password\":\""+pass+"\"}";
+        String value = "{\"account\":\"" + user + "\",\"password\":\"" + pass + "\"}";
         map.put("json", value);
 
-        Log.d("loginActivity", sessinStr + "      "+value );
+        Log.d("loginActivity", sessinStr + "      " + value);
 
 
         FastJsonRequest<LoginDataInfo> fastJsonCommunity = new FastJsonRequest<LoginDataInfo>(Request.Method.POST, url, LoginDataInfo.class, mapTou, new Response.Listener<LoginDataInfo>() {
@@ -180,14 +137,14 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
                 DataStatus status = loginDataInfo.getStatus();
                 if (status.getSucceed() == 1) {
                     loginDataActi = loginDataInfo.getData();
-                    if(loginDataActi!=null){
+                    if (loginDataActi != null) {
                         SharedPreferences.getInstance().putBoolean("islogin", true);
                         SharedPreferences.getInstance().putString("session", JSON.toJSONString(loginDataActi.getSession()));
-                        SharedPreferences.getInstance().putString("usename",user);
-                        SharedPreferences.getInstance().putString("password",pass);
-                        SharedPreferences.getInstance().putString("user",JSON.toJSONString(loginDataActi.getUser()));
-                       finish();
-                        Log.d("loginActivity", "登录信息：   "+JSON.toJSONString(loginDataActi.getSession())+"" + JSON.toJSONString(loginDataActi.getUser())+ "++++succeed");
+                        SharedPreferences.getInstance().putString("usename", user);
+                        SharedPreferences.getInstance().putString("password", pass);
+                        SharedPreferences.getInstance().putString("user", JSON.toJSONString(loginDataActi.getUser()));
+                        finish();
+                        Log.d("loginActivity", "登录信息：   " + JSON.toJSONString(loginDataActi.getSession()) + "" + JSON.toJSONString(loginDataActi.getUser()) + "++++succeed");
                     }
 
 
@@ -195,7 +152,7 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
 
                     // 请求失败
                     Log.d("loginActivity", "succeded=0  " + JSON.toJSONString(status) + "");
-                    Toast.makeText(getApplicationContext(),""+status.getError_desc(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "" + status.getError_desc(), Toast.LENGTH_SHORT).show();
 
                 }
 
@@ -205,7 +162,7 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
             @Override
             public void onErrorResponse(VolleyError volleyError) {
                 Log.d("loginActivity", "errror" + volleyError.toString() + "");
-                Toast.makeText(mContext,"服务器出错啦。。。",Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "服务器出错啦。。。", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -213,6 +170,56 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
 
         mQueue.add(fastJsonCommunity);
     }
+
+    @OnClick({R.id.weixinLogin, R.id.qqLogin, R.id.xinlanLogin})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.weixinLogin:
+                   weixinLoginMoth();
+                break;
+            case R.id.qqLogin:
+                break;
+            case R.id.xinlanLogin:
+                break;
+
+            case R.id.linearSeePassword:
+                if (!isShowPassword) {
+
+                    //如果选中，显示密码
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    isShowPassword = true;
+                } else {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    isShowPassword = false;
+                }
+                break;
+            case R.id.logon:
+                LoginBut(phone.getText().toString(), password.getText().toString(), "", "");
+                break;
+            case R.id.backBut:
+                finish();
+                break;
+            case R.id.regitstBut:
+                //跳转到注册界面
+                Intent intent = new Intent(mContext, QuaiRegitedActivity.class);
+              /*  Bundle bundle = new Bundle();
+                bundle.putSerializable("PromotePostsData", (Serializable) pp);
+                intent.putExtras(bundle);*/
+                mContext.startActivity(intent);
+                break;
+            case R.id.forgetPass:
+                //跳转到注册界面
+                Intent intent2 = new Intent(mContext, FindPasswordActivity.class);
+              /*  Bundle bundle = new Bundle();
+                bundle.putSerializable("PromotePostsData", (Serializable) pp);
+                intent.putExtras(bundle);*/
+                mContext.startActivity(intent2);
+                break;
+
+        }
+    }
+
+
 
     class EditChangedListener implements TextWatcher {
         private CharSequence temp;//监听前的文本
@@ -223,31 +230,37 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
         @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            if(s.length()==5){
+            if (s.length() == 5) {
                 logon.setBackground(getResources().getDrawable(R.drawable.login_button));
                 logon.setClickable(true);
             }
-            if(s.length()<5){
+            if (s.length() < 5) {
                 logon.setBackground(getResources().getDrawable(R.drawable.yuanjiao_love_red_bg_alpha));
                 logon.setClickable(false);
             }
-                Log.i(Tag, "输入文本之前的状态");
+            Log.i(Tag, "输入文本之前的状态");
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                Log.i(Tag, "输入文字中的状态，count是一次性输入字符数");
+            Log.i(Tag, "输入文字中的状态，count是一次性输入字符数");
 
         }
 
         @Override
         public void afterTextChanged(Editable s) {
-                Log.i(Tag, "输入文字后的状态");
+            Log.i(Tag, "输入文字后的状态");
             /** 得到光标开始和结束位置 ,超过最大数后记录刚超出的数字索引进行控制 */
 
 
         }
     }
 
+
+    /**
+     * 微信登录
+     */
+    private void weixinLoginMoth() {
+    }
 }
