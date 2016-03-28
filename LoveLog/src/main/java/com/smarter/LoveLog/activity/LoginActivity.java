d910bf756f4208dc97e24dd544b4fb751cb63c3d
luozhimin0918 <838111691@qq.com>
2016-03-28 17:15:21 +0800

    3月28 17.15

--- LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
+++ LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
@@ -307,7 +307,13 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
         /**通过这句代码，SDK实现了QQ的登录，这个方法有三个参数，第一个参数是context上下文，第二个参数SCOPO 是一个String类型的字符串，表示一些权限
          官方文档中的说明：应用需要获得哪些API的权限，由“，”分隔。例如：SCOPE = “get_user_info,add_t”；所有权限用“all”
          第三个参数，是一个事件监听器，IUiListener接口的实例，这里用的是该接口的实现类 */
-        mTencent.login(LoginActivity.this,"all", new BaseUiListener());
+
+
+        if (!mTencent.isSessionValid())
+        {
+            mTencent.login(LoginActivity.this,"all", new BaseUiListener());
+        }
+
     }
 
 
