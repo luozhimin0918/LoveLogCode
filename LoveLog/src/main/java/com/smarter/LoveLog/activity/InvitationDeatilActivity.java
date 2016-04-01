020c0308c80b3d8d189840e086b6d3904e16cbac
luozhimin0918 <838111691@qq.com>
2016-04-01 16:42:35 +0800

    4月1 16.42

--- LoveLog/src/main/java/com/smarter/LoveLog/activity/InvitationDeatilActivity.java
+++ LoveLog/src/main/java/com/smarter/LoveLog/activity/InvitationDeatilActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -28,6 +30,7 @@ import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -67,6 +70,7 @@ import com.smarter.LoveLog.model.goods.CmtGoods;
 import com.smarter.LoveLog.model.home.DataStatus;
 import com.smarter.LoveLog.model.jsonModel.ZanOrFaroviteParame;
 import com.smarter.LoveLog.model.loginData.SessionData;
+import com.smarter.LoveLog.shareSdk.ShareDialog;
 import com.smarter.LoveLog.ui.CircleNetworkImage;
 import com.smarter.LoveLog.ui.McoySnapPageLayout.McoyScrollView;
 import com.smarter.LoveLog.ui.QCheckBox;
@@ -84,12 +88,18 @@ import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.wechat.friends.Wechat;
+import cn.sharesdk.wechat.moments.WechatMoments;
 
 /**
  * Created by Administrator on 2015/11/30.
  */
-public class InvitationDeatilActivity extends BaseFragmentActivity implements View.OnClickListener{
+public class InvitationDeatilActivity extends BaseFragmentActivity implements View.OnClickListener,PlatformActionListener {
     String Tag= "InvitationDeatilActivity";
     Context  mContext;
 
@@ -446,16 +456,16 @@ public class InvitationDeatilActivity extends BaseFragmentActivity implements Vi
     private void addImageClickListner() {
         // 这段js函数的功能就是，遍历所有的img几点，并添加onclick函数，在还是执行的时候调用本地接口传递url过去
         webview.loadUrl("javascript:(function(){" +
-                " var srcs = [];"+
+                " var srcs = [];" +
                 "var objs = document.getElementsByTagName(\"img\"); " +
-                "var aList= document.getElementsByTagName(\"a\"); "+
-                "for(var i=0;i<objs.length;i++){"+
-                "srcs[i] = objs[i].src; }"+
+                "var aList= document.getElementsByTagName(\"a\"); " +
+                "for(var i=0;i<objs.length;i++){" +
+                "srcs[i] = objs[i].src; }" +
 
 
-                "for(var a=0;a<aList.length;a++){"+
-                " aList[a].onclick=function(){"+
-                " window.imagelistner.openWebview(this.href);}}"+
+                "for(var a=0;a<aList.length;a++){" +
+                " aList[a].onclick=function(){" +
+                " window.imagelistner.openWebview(this.href);}}" +
 
 
                 "for(var i=0;i<objs.length;i++)  " +
@@ -467,6 +477,9 @@ public class InvitationDeatilActivity extends BaseFragmentActivity implements Vi
                 "}" +
                 "})()");
     }
+
+
+
     // js通信接口
     public class JavascriptInterface {
 
@@ -666,11 +679,171 @@ public class InvitationDeatilActivity extends BaseFragmentActivity implements Vi
              case R.id.shareBut:
              case R.id.sharePic:
 
-                 showShare(promotePostsData);
+//                 showShare(promotePostsData);
+
+                   showShareMy(promotePostsData);
 
                  break;
          }
     }
+
+    ShareDialog shareDialog;
+    private void showShareMy(PromotePostsData promotePostsData) {
+
+        shareDialog = new ShareDialog(this);
+        shareDialog.setCancelButtonOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                shareDialog.dismiss();
+
+            }
+        });
+        shareDialog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int arg2, long arg3) {
+                HashMap<String, Object> item = (HashMap<String, Object>) arg0.getItemAtPosition(arg2);
+                if (item.get("ItemText").equals("微博")) {
+
+                  /*  //2、设置分享内容
+                    Platform.ShareParams sp = new Platform.ShareParams();
+                    sp.setText("我是分享文本，啦啦啦~http://uestcbmi.com/"); //分享文本
+                    sp.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
+
+                    //3、非常重要：获取平台对象
+                    Platform sinaWeibo = ShareSDK.getPlatform(SinaWeibo.NAME);
+                    sinaWeibo.setPlatformActionListener(MainActivity.this); // 设置分享事件回调
+                    // 执行分享
+                    sinaWeibo.share(sp);*/
+
+                } else if (item.get("ItemText").equals("微信好友")) {
+                    Toast.makeText(mContext, "您点中了" + item.get("ItemText"), Toast.LENGTH_LONG).show();
+
+                    //2、设置分享内容
+                    Platform.ShareParams sp = new Platform.ShareParams();
+                    sp.setShareType(Platform.SHARE_WEBPAGE);//非常重要：一定要设置分享属性
+                    sp.setTitle("我是分享标题");  //分享标题
+                    sp.setText("我是分享文本，啦啦啦~http://uestcbmi.com/");   //分享文本
+                    sp.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
+                    sp.setUrl("http://sharesdk.cn");   //网友点进链接后，可以看到分享的详情
+
+                    //3、非常重要：获取平台对象
+                    Platform wechat = ShareSDK.getPlatform(Wechat.NAME);
+                    wechat.setPlatformActionListener(InvitationDeatilActivity.this); // 设置分享事件回调
+                    // 执行分享
+                    wechat.share(sp);
+
+
+                } else if (item.get("ItemText").equals("朋友圈")) {
+                    //2、设置分享内容
+                    Platform.ShareParams sp = new Platform.ShareParams();
+                    sp.setShareType(Platform.SHARE_WEBPAGE); //非常重要：一定要设置分享属性
+                    sp.setTitle("我是分享标题");  //分享标题
+                    sp.setText("我是分享文本，啦啦啦~http://uestcbmi.com/");   //分享文本
+                    sp.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
+                    sp.setUrl("http://sharesdk.cn");   //网友点进链接后，可以看到分享的详情
+
+                    //3、非常重要：获取平台对象
+                    Platform wechatMoments = ShareSDK.getPlatform(WechatMoments.NAME);
+                    wechatMoments.setPlatformActionListener(InvitationDeatilActivity.this); // 设置分享事件回调
+                    // 执行分享
+                    wechatMoments.share(sp);
+
+                } else if (item.get("ItemText").equals("QQ")) {
+                    //2、设置分享内容
+                    Platform.ShareParams sp = new Platform.ShareParams();
+                    sp.setTitle("我是分享标题");
+                    sp.setText("我是分享文本，啦啦啦~http://uestcbmi.com/");
+                    sp.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");//网络图片rul
+                    sp.setTitleUrl("http://www.baidu.com");  //网友点进链接后，可以看到分享的详情
+                    //3、非常重要：获取平台对象
+                    Platform qq = ShareSDK.getPlatform(QQ.NAME);
+                    qq.setPlatformActionListener(InvitationDeatilActivity.this); // 设置分享事件回调
+                    // 执行分享
+                    qq.share(sp);
+
+                }
+
+
+                shareDialog.dismiss();
+
+            }
+        });
+    }
+
+
+    /**
+     * PlatformActionListener   onComplete onError onCancel
+     * @param platform
+     * @param i
+     * @param hashMap
+     */
+    @Override
+    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+       /* if (platform.getName().equals(SinaWeibo.NAME)) {// 判断成功的平台是不是新浪微博
+            handler.sendEmptyMessage(1);
+        } else*/ if (platform.getName().equals(Wechat.NAME)) {
+            handler.sendEmptyMessage(1);
+        } else if (platform.getName().equals(WechatMoments.NAME)) {
+            handler.sendEmptyMessage(3);
+        } else if (platform.getName().equals(QQ.NAME)) {
+            handler.sendEmptyMessage(4);
+        }
+
+    }
+
+    @Override
+    public void onError(Platform platform, int i, Throwable throwable) {
+        throwable.printStackTrace();
+        Message msg = new Message();
+        msg.what = 6;
+        msg.obj = throwable.getMessage();
+        handler.sendMessage(msg);
+    }
+
+    @Override
+    public void onCancel(Platform platform, int i) {
+        handler.sendEmptyMessage(5);
+    }
+
+    /**
+     * 分享完的回调handler
+     */
+    Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Toast.makeText(getApplicationContext(), "微博分享成功", Toast.LENGTH_LONG).show();
+                    break;
+
+                case 2:
+                    Toast.makeText(getApplicationContext(), "微信分享成功", Toast.LENGTH_LONG).show();
+                    break;
+                case 3:
+                    Toast.makeText(getApplicationContext(), "朋友圈分享成功", Toast.LENGTH_LONG).show();
+                    break;
+                case 4:
+                    Toast.makeText(getApplicationContext(), "QQ分享成功", Toast.LENGTH_LONG).show();
+                    break;
+
+                case 5:
+                    Toast.makeText(getApplicationContext(), "取消分享", Toast.LENGTH_LONG).show();
+                    break;
+                case 6:
+                    Toast.makeText(getApplicationContext(), "分享失败啊" + msg.obj, Toast.LENGTH_LONG).show();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
+
 
     /**
      * 分享内容方法
