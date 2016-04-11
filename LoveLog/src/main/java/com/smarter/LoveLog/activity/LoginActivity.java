720a10d03081ed93dcec18e2e45c325735123ed0
luozhimin0918 <838111691@qq.com>
2016-04-11 13:52:20 +0800

    4月11 13.52

--- LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
+++ LoveLog/src/main/java/com/smarter/LoveLog/activity/LoginActivity.java
@@ -119,13 +119,7 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
 
 
 
-        /**
-         * tententQQ
-         */
-        //这里的APP_ID请换成你应用申请的APP_ID，我这里使用的是DEMO中官方提供的测试APP_ID 222222
-        mAppid = ConstantsQQ.APP_ID;
-        //第一个参数就是上面所说的申请的APPID，第二个是全局的Context上下文，这句话实现了调用QQ登录
-        mTencent = Tencent.createInstance(mAppid,getApplicationContext());
+
 
 
 
@@ -210,7 +204,6 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
                    weixinLoginMoth();
                 break;
             case R.id.qqLogin:
-//                   tennetLoginMoth();
                 platform = SHARE_MEDIA.QQ;
                 weixinLoginMoth();
                 break;
@@ -335,100 +328,11 @@ public class LoginActivity extends BaseFragmentActivity implements View.OnClickL
         }
     };
 
-    /**
-     * QQ登录
-     */
-   public static  Tencent mTencent;
-    public static String mAppid;
-    private void tennetLoginMoth() {
-
-        /**通过这句代码，SDK实现了QQ的登录，这个方法有三个参数，第一个参数是context上下文，第二个参数SCOPO 是一个String类型的字符串，表示一些权限
-         官方文档中的说明：应用需要获得哪些API的权限，由“，”分隔。例如：SCOPE = “get_user_info,add_t”；所有权限用“all”
-         第三个参数，是一个事件监听器，IUiListener接口的实例，这里用的是该接口的实现类 */
-
-
-        if (!mTencent.isSessionValid())
-        {
-            mTencent.login(LoginActivity.this,"all", new BaseUiListener());
-        }
-
-    }
-
-
-
-    /**当自定义的监听器实现IUiListener接口后，必须要实现接口的三个方法，
-     * onComplete  onCancel onError
-     *分别表示第三方登录成功，取消 ，错误。*/
-    public static String openidString;
-    private class BaseUiListener implements IUiListener {
-
-        public void onCancel() {
-            // TODO Auto-generated method stub
-
-        }
-
-        public void onComplete(Object response) {
-            // TODO Auto-generated method stub
-            Toast.makeText(getApplicationContext(), "登录成功",Toast.LENGTH_SHORT).show();
-            try {
-                //获得的数据是JSON格式的，获得你想获得的内容
-                //如果你不知道你能获得什么，看一下下面的LOG
-                Log.e(Tag, "-------------" + response.toString());
-
-                    openidString = ((JSONObject) response).getString("openid");
-
-
-                Log.e(Tag, "-------------" + openidString);
-                //access_token= ((JSONObject) response).getString("access_token");
-                // expires_in = ((JSONObject) response).getString("expires_in");
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            /**到此已经获得OpneID以及其他你想获得的内容了
-             QQ登录成功了，我们还想获取一些QQ的基本信息，比如昵称，头像什么的，这个时候怎么办？
-             sdk给我们提供了一个类UserInfo，这个类中封装了QQ用户的一些信息，我么可以通过这个类拿到这些信息
-             如何得到这个UserInfo类呢？  */
-            QQToken qqToken = mTencent.getQQToken();
-            UserInfo info = new UserInfo(getApplicationContext(), qqToken);
-            //这样我们就拿到这个类了，之后的操作就跟上面的一样了，同样是解析JSON
-        }
-
-        @Override
-        public void onError(UiError uiError) {
-
-        }
-
-    }
 
 
 
 
-  /*  @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
 
-        setIntent(intent);
-        MainActivity.api.handleIntent(intent, this);
-    }
-    @Override
-    public void onReq(BaseReq baseReq) {
-
-    }
-
-    @Override
-    public void onResp(BaseResp baseResp) {
-        Log.d("loginActivity","onRespBase"+baseResp.errCode);
-
-
-        if (baseResp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("提示");
-            builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(baseResp.errCode)));
-            builder.show();
-        }
-
-    }*/
 
 
 
